491ad369591dc4507742a1013f1e903fe23191dd
Mandar Dalvi <Mandar Dalvi@MandarDalvi-PC>
2014-02-11 19:41:06 -0700

    Task 1 Completed

--- DBI/src/global/GlobalConst.java
+++ DBI/src/global/GlobalConst.java
@@ -3,15 +3,15 @@ package global;
 public interface GlobalConst {
 
   public static final int MINIBASE_MAXARRSIZE = 50;
-  public static final int NUMBUF = 50;
+  public static final int NUMBUF = 4000;
 
   /** Size of page. */
-  public static final int MINIBASE_PAGESIZE = 1024;           // in bytes
+  public static final int MINIBASE_PAGESIZE = 4096;           // in bytes
 
   /** Size of each frame. */
-  public static final int MINIBASE_BUFFER_POOL_SIZE = 1024;   // in Frames
+  public static final int MINIBASE_BUFFER_POOL_SIZE = 4096;   // in Frames
 
-  public static final int MAX_SPACE = 1024;   // in Frames
+  public static final int MAX_SPACE = 4096;   // in Frames
   
   /**
    * in Pages => the DBMS Manager tells the DB how much disk 
